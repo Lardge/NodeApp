e02aeaaf5c9fbfe26afdbd7327233f17c3bed9d5
Lardge <larsbf@gmail.com>
2017-08-31 18:18:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const app = express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/test.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/test.html'));
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+})
