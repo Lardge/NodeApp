d29473ab120b2e917ffeac1a01f5edd2e1f1bf28
Lardge <larsbf@gmail.com>
2017-09-11 18:47:03 +0200

    node modules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const app = express();
+var crawler = require("crawler");
 
 /*var con = mysql.createConnection({
   host: "localhost",
@@ -25,3 +26,58 @@ app.get('/test.html', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 })
+
+var c = new crawler({
+    maxConnections : 10,
+    rateLimit: 1000,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
+
+// Queue just one URL, with default callback
+c.queue('http://www.avanza.se');
+
+// Queue a list of URLs
+//c.queue(['http://www.google.com/','http://www.yahoo.com']);
+
+// Queue URLs with custom callbacks & parameters
+/*c.queue([{
+    uri: 'http://parishackers.org/',
+    jQuery: false,
+
+    // The global callback won't be called
+    callback: function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Grabbed', res.body.length, 'bytes');
+        }
+        done();
+    }
+}]);*/
+
+// Queue some HTML code directly without grabbing (mostly for tests)
+c.queue([{
+    html: '<p>This is a <strong>test</strong></p>'
+}]);
+
+var schedule = require('node-schedule');
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 9;
+rule.minute = 0;
+ 
+var j = schedule.scheduleJob(rule, function(){
+  console.log('The world is going to end today.');
+    c.queue('http://www.avanza.se');
+});
